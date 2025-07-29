3763f8a22dd700a4f0def8bd6c8fc9c26f1c8de1
Yuyukoi <misaka_yyz@icloud.com>
2025-07-30 00:31:58 +0800

    homepage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import { Route, Routes } from 'react-router-dom';
-// import LoginPage from './components/LoginPage';
-// import UserInformationPage from './components/UserInformationPage';
-
-// const App = () => {
-//   return (
-    
-//     <Routes>
-//       <Route path="/" element={<LoginPage />} />  {/* 主页面路由 */}
-//       <Route path="/callback" element={<LoginPage />} />  {/* 新增回调路由 */}
-//       <Route path="/userinformation" element={<UserInformationPage />} />  {/* 用户信息页面 */}
-//     </Routes>
-//   );
-// };
-
-// export default App;
-
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';  // 导入 Header 组件
@@ -30,7 +12,7 @@ const App = () => {
 
       {/* 添加路由管理 */}
       <Routes>
-        <Route path="/" element={<LoginPage />} />  {/* 主页面路由 */}
+        <Route path="/bytebase-login-page" element={<LoginPage />} />  {/* 主页面路由 */}
         <Route path="/callback" element={<LoginPage />} />  {/* 回调路由 */}
         <Route path="/userinformation" element={<UserInformationPage />} />  {/* 用户信息页面 */}
       </Routes>
